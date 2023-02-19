03383f250c356c3276bd916c2cadc8d0da8631fd
James <72478345+wikssjs@users.noreply.github.com>
2023-02-18 22:52:24 -0500

    a

--- component/Links.jsx
+++ component/Links.jsx
@@ -13,7 +13,7 @@ export default function Links() {
         <ul className={styles.liste}>
             <li> <p>Suivez moi</p></li>
             <span></span>
-            <li><a target="_blank" rel="noreferrer" href="https://github.com/wikssjs/wikssjs"><i className="bi bi-github"></i></a></li>
+            <li><a target="_blank" rel="noreferrer" href="https://github.com/wikssjs"><i className="bi bi-github"></i></a></li>
             <li><a target="_blank" rel="noreferrer" href="#"><i className="bi bi-instagram"></i></a></li>
             <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/james-bell-199ba9222/"><i className="bi bi-linkedin"></i></a></li>
         </ul>
