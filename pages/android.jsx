07ed16f01299a25b8d46a91392bf2bf9f4834d08
James <72478345+wikssjs@users.noreply.github.com>
2023-02-28 14:24:28 -0500

    ajout des meta donnes

--- pages/android.jsx
+++ pages/android.jsx
@@ -7,6 +7,7 @@ import todo from '../public/todo.jpg'
 import todo2 from '../public/todo.webp'
 import ProjetElement from "../component/ProjetElement"
 import ProjetGrid from "../component/ProjetGrid"
+import Head from "next/head"
 
 
 /**
@@ -16,10 +17,20 @@ import ProjetGrid from "../component/ProjetGrid"
 export default function Projet1() {
 
 
-  return <ProjetGrid titre='Android' icon="bi bi-android2">
-    <ProjetElement image={ecommerce2} type="Android Developement" nom="ECommerce  App" id={0} anim="animate__animated animate__bounceInLeft" video="/videos/ecommerce.mp4" github="https://github.com/wikssjs/JamesEcommerce" languages={["JAVA -", "ANDROID STUDIO -","FIREBASE -", "API"]} />
-    <ProjetElement image={blackjack2} type="Android Developement" nom="Blackjack game App" id={1} anim="animate__animated animate__bounceInUp" video="/videos/blackjack.mp4" github="https://github.com/wikssjs/Blackjack" languages={["JAVA -", "ANDROID STUDIO"]} />
-    <ProjetElement image={todo2} type="Android Developement" nom="Todo  App" id={2} anim="animate__animated animate__bounceInRight" video="/videos/todo.mp4" github="https://github.com/wikssjs/TODO_APP" languages={["JAVA -", "ANDROID STUDIO -", "SQLITE"]} />
-  </ProjetGrid>
+  return <>
+    <Head>
+      <title>JamesBell - Contact</title>
+      <meta name="description" content="Page de projet Android de mon portfolio" />
+      <meta property="og:title" content="JamesBell - Projet-Android" />
+      <meta property="og:description" content="Page de Projet Android de mon portfolio" />
+      <meta property="og:image" content="https://jamesbell.vercel.app/james.png" />
+    </Head>
+    <ProjetGrid titre='Android' icon="bi bi-android2">
+      <ProjetElement image={ecommerce2} type="Android Developement" nom="ECommerce  App" id={0} anim="animate__animated animate__bounceInLeft" video="/videos/ecommerce.mp4" github="https://github.com/wikssjs/JamesEcommerce" languages={["JAVA -", "ANDROID STUDIO -", "FIREBASE -", "API"]} />
+      <ProjetElement image={blackjack2} type="Android Developement" nom="Blackjack game App" id={1} anim="animate__animated animate__bounceInUp" video="/videos/blackjack.mp4" github="https://github.com/wikssjs/Blackjack" languages={["JAVA -", "ANDROID STUDIO"]} />
+      <ProjetElement image={todo2} type="Android Developement" nom="Todo  App" id={2} anim="animate__animated animate__bounceInRight" video="/videos/todo.mp4" github="https://github.com/wikssjs/TODO_APP" languages={["JAVA -", "ANDROID STUDIO -", "SQLITE"]} />
+    </ProjetGrid>
+  </>
+
 
 }
--- pages/apropos.jsx
+++ pages/apropos.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import AproposContenu from '../component/AproposContenu';
 import styles from '../styles/Apropos.module.css'
 import Education from '../component/Education';
+import Head from 'next/head';
 export default function Apropos() {
 
         const [anim, addAnim] = useState(false);
@@ -10,9 +11,20 @@ export default function Apropos() {
         const changeAddAnim = () => {
                 addAnim(!anim)
         }
-        return <main className={`${styles.main} d-flex flex-column gap-5 container-lg shadow-lg rounded-5 p-3`}>
+        return <>
+         <Head>
+        <title>JamesBell - Contact</title>
+        <meta name="description" content="Page Apropos de mon portfolio" />
+        <meta property="og:title" content="JamesBell - Apropos"/>
+        <meta property="og:description" content="Page Appropos de mon portfolio"/>
+        <meta property="og:image" content="https://jamesbell.vercel.app/james.png"/>
+        
+        </Head>
+        <main className={`${styles.main} d-flex flex-column gap-5 container-lg shadow-lg rounded-5 p-3`}>
                <AproposContenu/>
                <Education/>
         </main>
+        </>
+        
 
 }
