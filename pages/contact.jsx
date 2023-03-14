4d52dd76495922b44b93ff7e4d3d73289f2210a5
James <72478345+wikssjs@users.noreply.github.com>
2023-03-13 21:44:16 -0400

    add validation to the contact form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,21 +1,87 @@
 import styles from '../styles/Contact.module.css'
 import Head from 'next/head'
+import { useState } from 'react'
 
 
 
 
 export default function Contact() {
+    const [courriel, setCourriel] = useState('');
+    const handleCourrielChange = (event) => setCourriel(event.target.value);
+    const [erreurCourriel, setErreurCourriel] = useState('');
+    const valideCourriel = () => {
+
+        if (!courriel) {
+            setErreurCourriel('Le courriel est requis');
+            return false;
+        }
+        else if (!courriel.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i)) {
+            setErreurCourriel('Le courriel n\'est pas valide');
+            return false;
+        }
+        else {
+            setErreurCourriel('');
+            return true;
+        }
+    }
+
+    const [nom, setNom] = useState('');
+    const [erreurNom, setErreurNom] = useState('');
+    const handleNomChange = (event) => setNom(event.target.value);
+    const validateNom = () => {
+        if (!nom) {
+            setErreurNom('Le nom est requis');
+            return false;
+        }
+
+        else {
+            setErreurNom('');
+            return true;
+        }
+    }
+
+
+    const [message, setMessage] = useState('');
+    const [erreurMessage, setErreurMessage] = useState('');
+    const handleMessageChange = (event) => setMessage(event.target.value);
+    const validateMessage = () => {
+        if (!message) {
+            setErreurMessage('Le message est requis');
+            return false;
+        }
+
+        else {
+            setErreurMessage('');
+            return true;
+        }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+
+        let courrielValide = valideCourriel();
+        let nomValide = validateNom();
+        let messageValide = validateMessage();
+
+
+        if (courrielValide && nomValide && messageValide) {
+            event.currentTarget.action = 'https://formspree.io/f/mlekbgol';
+            event.currentTarget.method = 'POST';
+            event.currentTarget.submit();
+        }
+    }
 
 
     return <>
-     <Head>
-        <title>JamesBell - Contact</title>
-        <meta name="description" content="Page de Contact de mon portfolio" />
-        <meta property="og:title" content="JamesBell - Contact"/>
-        <meta property="og:description" content="Page de Contact de mon portfolio"/>
-        <meta property="og:image" content="https://jamesbell.vercel.app/james.png"/>
-        
-    </Head>
+        <Head>
+            <title>JamesBell - Contact</title>
+            <meta name="description" content="Page de Contact de mon portfolio" />
+            <meta property="og:title" content="JamesBell - Contact" />
+            <meta property="og:description" content="Page de Contact de mon portfolio" />
+            <meta property="og:image" content="https://jamesbell.vercel.app/james.png" />
+
+        </Head>
         <main className={`${styles.container}  d-flex flex-column align-items-center container gap-5 pb-5 shadow-lg rounded-5 container-lg p-3  gap-5 mb-5 d-flex `}>
             <h1>Contact</h1>
             <div className='w-75  animate__animated animate__bounceInLeft'>
@@ -23,21 +89,37 @@ export default function Contact() {
             </div>
 
             <div className={`${styles.contact_wrapper} w-100 d-flex flex-column align-items-center  animate__animated animate__bounceInLeft`}>
-                <form target='_blank' action="https://formspree.io/f/mlekbgol" method='POST' className={`${styles.contact_form} d-flex flex-column`}>
+                <form onSubmit={handleSubmit}  className={`${styles.contact_form} d-flex flex-column`} noValidate>
                     <h2>Contactez-Moi</h2>
                     <label>
                         Nom
-                        <input type="text" required name='name' />
+                        <input type="text" required name='name' onChange={handleNomChange} onBlur={validateNom} />
                     </label>
+                    {erreurNom &&
+                        <div className={styles.erreur}>
+                            {erreurNom}
+                        </div>
+                    }
                     <label htmlFor="">
                         EMAIL
-                        <input type="email" name="email" id="" required />
+                        <input type="email" name="email" id="" required onChange={handleCourrielChange} onBlur={valideCourriel} />
 
                     </label>
+
+                    {erreurCourriel &&
+                        <div className={styles.erreur}>
+                            {erreurCourriel}
+                        </div>
+                    }
                     <label htmlFor="">
                         Message
-                        <textarea name="message" id="" cols="30" rows="10" required></textarea>
+                        <textarea onChange={handleMessageChange} onBlur={validateMessage} name="message" id="" cols="30" rows="10" required></textarea>
                     </label>
+                    {erreurMessage &&
+                        <div className={styles.erreur}>
+                            {erreurMessage}
+                        </div>
+                    }
                     <input className='d-none' name='bot-field' />
                     <button type='submit'>Envoyer <i className='bi bi-arrow-right'></i></button>
                 </form>
